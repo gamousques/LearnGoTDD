686647ee4c0cb8d1aea932343f3a08b8b35c6160
gamousques <mousques@hotmail.com>
2022-01-24 12:28:10 -0300

    variadic functions, uses os slices, and deepequla to compare

--- arraySlices/sum.go
+++ arraySlices/sum.go
@@ -1,10 +1,21 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum :=0
 
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(slices ...[]int ) []int {
+	numberOfSlices := len(slices);
+	sums := make([]int, numberOfSlices)
+
+	for i, numbers := range slices {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums;
 }
