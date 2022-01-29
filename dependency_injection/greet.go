067a709426fc0f20ac0adda34639e5b3ff2b9c40
gamousques <mousques@hotmail.com>
2022-01-29 15:59:03 -0300

    addded main funct

--- dependency_injection/greet.go
+++ dependency_injection/greet.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(out *bytes.Buffer, name string) {
-	fmt.Fprintf(out, "Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+
+func main() {
+	Greet(os.Stdout, "Gastón")
 }
